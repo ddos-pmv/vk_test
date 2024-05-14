84aabf600661757004ca2b062fd9b38309e8a3bc
ddos-pmv <perlinsergey@gmail.com>
2024-05-14 20:42:49 +0300

    added cache for product_price_{product_id}_{price_id}

---
+++ backend/app/Console/Commands/RedisTestCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+
+class RedisTestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'redis:go';
+
+    /**
+     * The console command description. a
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Cache::put('example',Json (['hello'=>'wold']));
+        //
+    }
+}
--- backend/app/Http/Controllers/Good/IndexController.php
+++ backend/app/Http/Controllers/Good/IndexController.php
@@ -8,31 +8,36 @@ use Illuminate\Http\Request;
 use App\Models\Good;
 use App\Models\Price;
 use App\Models\Region;
+use Illuminate\Support\Facades\Cache;
 
 class IndexController extends Controller
 {
     public function get($good_id, $region_id){
-        $product = Good::find($good_id);
-        if (! $product) {
-            return response()->json(['error' => 'Product not found'], 404);
-        }
-        $productName = $product->name;
-        $price = Price::where(['good_id' => $good_id, 'region_id'=>$region_id])->first();
-        if (! $price) {
-            return response()->json(['error' => 'Price not found for this region'], 404);
-        }
-        $regionPrice = $price->value;
+        $cacheKey = "product_price_{$good_id}_{$region_id}";
+        $data = Cache::remember($cacheKey, 60*60*60, function() use ($good_id, $region_id){
+            $product = Good::find($good_id);
+            if (! $product) {
+                return response()->json(['error' => 'Product not found'], 404);
+            }
+            $productName = $product->name;
+            $price = Price::where(['good_id' => $good_id, 'region_id'=>$region_id])->first();
+            if (! $price) {
+                return response()->json(['error' => 'Price not found for this region'], 404);
+            }
+            $regionPrice = $price->value;
 
-        $regionName = Region::find($region_id);
+            $regionName = Region::find($region_id);
 
-        // Return response with product name and price
-        return response()->json([
-            'product_name' => $productName,
-            'region_price' => $regionPrice,
-            'region_name' =>$regionName->name,
-            'product_quantity' => $product->quantity,
+            return [
+                'product_name' => $productName,
+                'region_price' => $regionPrice,
+                'region_name' =>$regionName->name,
+                'product_quantity' => $product->quantity,
 
-        ]);
+            ];
+        });
+        return response()->json($data);
+
 
     }
     public function all(){
